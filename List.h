d7619b8978fefc3bbd87eb97fe73dbe75f4178fe
mlmonesc <mmonerrise@gmail.com>
2023-11-08 20:07:40 +0100

    List.h

--- List.h
+++ List.h
@@ -1,12 +1,18 @@
-template <typename T>
-class List {
-    public:
-       void insert(int pos, T e)= 0;
-       void append (T e)= 0;
-       void prepend (T e)= 0;
-       T remove(int pos)= 0;
-       T get(int pos)= 0;
-       int search(T e)= 0;
-       bool empty()= 0;
-       int size= 0;
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+#include <ostream>
+
+template<typename T>
+class List{
+        public:
+        virtual void insert(int pos, T e)=0;
+        virtual void append(T e)=0;
+        virtual void prepend(T e)=0;
+        virtual T remove(int pos)=0;
+        virtual T get (int pos)=0;
+        virtual int search(T e)=0;
+        virtual bool empty()=0;
+        virtual int size()=0;
 };
+#endif
